dda068370db3673c800f73b9bb2eac4902a53cd7
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2023-08-18 14:07:45 +0100

    first commit

---
+++ src/bookData.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const options = [
+  "Harry Potter and the Philosopher's Stone",
+  "Harry Potter and the Chamber of Secrets",
+  "Harry Potter and the Prisoner of Azkaban",
+  "Harry Potter and the Goblet of Fire",
+  "Harry Potter and the Order of the Phoenix",
+  "Harry Potter and the Half-Blood Prince",
+  "Harry Potter and the Deathly Hallows",
+  "James and the Giant Peach",
+  "Matilda",
+  "The BFG",
+  "Twilight",
+  "New Moon",
+  "Eclipse",
+  "Breaking Dawn",
+  "The Secret",
+  "The Lie",
+  "Into the Water",
+  "From the Shadows",
+  "The Girl with the Dragon Tattoo",
+  "A Spark of Light",
+];
+
+export default options;
---
+++ src/books.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import options from "./bookData";
+
+function BookList() {
+  const [selected, setSelected] = useState([]);
+  function addBook(e) {
+    let selectedBook = e.target.value;
+    setSelected((prev) => {
+      return [...prev, selectedBook];
+    });
+
+    e.target.disabled = true;
+  }
+
+  function removeBook(e) {
+    let selectedBook = e.target.innerHTML;
+
+    document.getElementById(selectedBook).disabled = false;
+
+    setSelected((prev) => {
+      return prev.filter((book) => book !== selectedBook);
+    });
+  }
+
+  return (
+    <div>
+      <h1>Library rentals</h1>
+      <div id="list">
+        <h2>Books on your reading list:</h2>
+        <p>
+          Click on a book to remove it from your reading list. If there is
+          nothing on your reading list yet, look in the library section below to
+          see what you'd like to add.
+        </p>
+        <ul>
+          {selected.map((book) => {
+            return (
+              <li value={book} key={book} onClick={removeBook}>
+                {book}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div id="library">
+        <h2>Books available in library:</h2>
+        <p>Click on a book to add it to your reading list.</p>
+        <div id="buttons">
+          {options.map((option) => {
+            return (
+              <button
+                disabled={false}
+                value={option}
+                key={option}
+                id={option}
+                onClick={addBook}
+              >
+                {option}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookList;
